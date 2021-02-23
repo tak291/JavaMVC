ca83660cb7e2bb4b998f6ac7bb6e27a47034d69b
jose <joseporras291@gmail.com>
2021-02-22 23:28:36 -0500

    Un paso más. Ya vemos el contenido del arreglo

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -7,12 +7,12 @@ public class Controlador {
 
 	//Iniciamos la vista y los arreglos.
 	Vista V;
-	Arreglos Apellidos;
+	Arreglos Apellidos ;
 	Arreglos Fechas;
 	Arreglos Tels;
 	
-	
-	String Apellido [] = {"La verdadera", "La excitante", "La horripilante", "La alegre", " La fatal", "La pasionante", "La trágica", "La sangrienta", "La famosa", "La Gran", "La penosa", "La Triste", "La desastrosa", "La Hermosa", "La oscura", "La gloriosa", "La infeliz", "La emocionante", "La fantástica", "La terrorífica", "La aburrida", "La inreíble", "La Desraciada", "La lamentable", "La Cómica", "La miserable" };
+	//Arrays con la información a mostrar.
+	String Apellido [] = {"La verdadera", "La excitante", "La horripilante", "La alegre", "La fatal", "La pasionante", "La trágica", "La sangrienta", "La famosa", "La Gran", "La penosa", "La Triste", "La desastrosa", "La Hermosa", "La oscura", "La gloriosa", "La infeliz", "La emocionante", "La fantástica", "La terrorífica", "La aburrida", "La inreíble", "La Desraciada", "La lamentable", "La Cómica", "La miserable" };
 	String Fecha []= {"Hazaña", "Reencarnación", "Batalla", "Existencia", "Muerte", "Misión", "Venganza", "Anécdota", "Vida", "Leyenda", "historia", "Biografía"};
 	String Tel [] = {"De un bipolar", "De un asesino", "De un loco/a","De un adecito/a a los memes", "De un soñador/a", "De un genio incomprendido/a", "De un simbolo sexual", "De un alcohólico", "De un guerrero/a", "De un aventurero/a" };
 	
@@ -30,11 +30,14 @@ public class Controlador {
 		Fechas = new Arreglos(Fecha);
 		Tels = new Arreglos(Tel);
 		
-		System.out.println(Apellidos.getC()[3]);
+		//For loop para recorrer el areglo e imprimir el contenido.
+		for(int x = 0; x < Apellido.length; x++) {
+			System.out.println(Apellidos.getB()[x]);
+		}
 		
+		
+				
 		
 	}
-	
-
-	
+		
 }
